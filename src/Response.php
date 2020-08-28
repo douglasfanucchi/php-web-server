cf0992f606be3501edd8e7b715bfd32f604a54d1
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-08-28 12:52:04 -0300

    Implements addHeader method

--- src/Response.php
+++ src/Response.php
@@ -6,10 +6,16 @@ class Response
 {
   private string $responseJson;
   private int $statusCode;
+  private array $headers;
 
   public function __construct(\stdClass $responseData, int $statusCode)
   {
     $this->responseJson = json_encode($responseData);
     $this->statusCode = $statusCode;
+  }
+
+  public function addHeader(string $key, string $value)
+  {
+    $this->headers[$key] = $value;
   }
 }
