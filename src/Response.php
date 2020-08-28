ac2e5233bc9307dd7c653598032dc49c56f14e3f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-08-28 13:03:40 -0300

    Implements getRawHeader

--- src/Response.php
+++ src/Response.php
@@ -22,7 +22,7 @@ class Response
   public function __toString()
   {
     $httpInfo = "HTTP/1.1 " . $this->getStatusCodeInfo();
-    $header   = $this->getRawHeader();
+    $header   = $this->getRawHeader($this->headers);
     $body     = PHP_EOL . $this->responseJson;
 
     return $httpInfo . $header . $body;
@@ -36,5 +36,12 @@ class Response
       case 500:
         return '500 ERROR';
     }
+  }
+
+  private function getRawHeader(array $headers)
+  {
+    return array_reduce(array_keys($headers), function ($store, $key) use ($headers) {
+      return $store . "{$key}: {$headers[$key]}" . PHP_EOL;
+    }, '');
   }
 }
